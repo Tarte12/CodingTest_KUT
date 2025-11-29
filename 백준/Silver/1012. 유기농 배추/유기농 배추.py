7c894bc98de1104f61cfe68b612058b552bca719
Tarte <101166683+Tarte12@users.noreply.github.com>
2025-11-29 13:58:24 +0900

    [Silver II] Title: 유기농 배추, Time: 216 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,56 +1,43 @@
 from collections import deque
 
-#입력 받는 조건
-T = int(input())
-
-
-#방향 리스트 => 상하좌우
 dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dy = [1, -1, 0, 0]
 
-#2차원부터는 좌표로 체크?
-def bfs(x, y, N, M):
-  queue = deque()
-  queue.append((x, y))
-  visited[y][x] = True #시작점 방문 처리
+def bfs(x, y):      # (x,y): (가로,세로)
+    que = deque()
+    que.append((x, y))
+    visited[y][x] = True   # (y,x)
 
-  while queue: 
-    x, y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+    while que:
+        x, y = que.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-      if nx < 0 or nx >= N or ny < 0 or ny >= M:
-        continue
-      if not visited[ny][nx] and field[ny][nx] == 1:
-          visited[ny][nx] = True
-          queue.append((nx, ny))
-  
+            # 범위 체크 (x는 가로, y는 세로)
+            if 0 <= nx < m and 0 <= ny < n:
+                if not visited[ny][nx] and map_lst[ny][nx] == 1:
+                    visited[ny][nx] = True
+                    que.append((nx, ny))
 
-#테스트 조건 만큼 체크해야 함
-for _ in range(T):
-  M, N, K = map(int, input().split())
-  #배추밭 가로길이 M, 세로 길이 N, 배추 개수 K
-  #배추밭 생성 + 아직 배추 없다고 생각하고 0으로 초기화
-  field = [[0] * M for _ in range(N)]
+t = int(input())
 
-  visited = [[False] * M for _ in range(N)]
+for _ in range(t):
+    m, n, k = map(int, input().split())  # m=가로, n=세로
 
-  #베추 위치 <- K개
-  for _ in range(K):
-    x, y = map(int, input().split())
-    #배추 좌표 입력
-    field[y][x] = 1 #배추 심었음 -> 1인 부분만 배추라고 판단해야 하니까
+    # map[y][x] 형태로 생성
+    map_lst = [[0]*m for _ in range(n)]
+    visited = [[False]*m for _ in range(n)]
 
-  #인접한 배추 한 뭉치  => bfs 한 사이클
-  #한 사이클 count => 배추흰지렁이 마릿수
+    for _ in range(k):
+        x, y = map(int, input().split())  # (x:가로, y:세로)
+        map_lst[y][x] = 1
 
-  count = 0 #배추흰지렁이 마릿수
+    cnt = 0
+    for y in range(n):        # 세로 먼저
+        for x in range(m):    # 가로
+            if map_lst[y][x] == 1 and not visited[y][x]:
+                bfs(x, y)
+                cnt += 1
 
-  for i in range(N):  # 세로줄 순회 (위에서 아래로)
-   for j in range(M):  # 가로줄 순회 (왼→오)
-      if field[i][j] == 1 and not visited[i][j]:
-          bfs(j, i, M, N)  # bfs(x, y)
-          count += 1  # 새로운 무더기 발견
-  
-  print(count)
+    print(cnt)
