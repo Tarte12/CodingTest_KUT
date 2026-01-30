a9b583b4049aa17159c990bb218fff4bbaa3121f
KimHuiSu <101166683+Tarte12@users.noreply.github.com>
2026-01-30 15:33:08 +0900

    [Silver II] Title: 부분수열의 합, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,13 +1,20 @@
-from itertools import combinations
-
 n, s = map(int, input().split())
-nums = list(map(int, input().split()))
-
+lst = list(map(int, input().split()))
 cnt = 0
 
-for r in range(1, n + 1):        # 공집합 제외
-    for comb in combinations(nums, r):
-        if sum(comb) == s:
+def dfs(idx, sum):
+    global cnt
+    if idx == n:
+        if sum == s:
             cnt += 1
-
+        return
+    
+    #1. 현재 숫자를 더할 때
+    dfs(idx + 1, sum + lst[idx])
+    #2. 현재 숫자를 안 더할 때
+    dfs(idx + 1, sum)
+dfs(0, 0)
+if s == 0:
+    cnt -= 1
+    
 print(cnt)
